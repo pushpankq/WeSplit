52c66acb8d0d8c7e32b4ad7c859bb8a23bd6cfae
pushpank <pushpankq@gmail.com>
2024-03-29 21:08:34 +0530

    first commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Pushpank Kumar on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
